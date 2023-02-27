cf47a7b9320a48bfea85bf7066d79042067f5773
JungSol <solhsw@naver.com>
2023-02-27 22:27:53 +0900

    first commit

---
+++ src/main/java/com/codingrecipe/project01/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.codingrecipe.project01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")/*가장 기본 주소에 대한 설정*/
+    public String index() {
+        return "index"; // -> index.jsp 출력하겠다는 의미
+    }
+}
